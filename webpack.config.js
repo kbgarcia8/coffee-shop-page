3b52cd6dc34b0634a2ede3de42a21c90c5600d86
kbgarcia8 <kbgarcia8@users.noreply.github.com>
2024-04-16 03:20:23 +0800

    update coffee shop project (webpack practice)

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 const path = require('path');
-//const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   watch: true,
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'images/[hash][ext][query]',
   },
   module: {
     rules: [
